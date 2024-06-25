9d21127d66baa069259698674d761124070c75d0
quockhanhit <quockhanhit321@gmail.com>
2024-06-25 09:05:22 -0700

    quockhanh

--- RestfullAPINET/Controllers/WeatherForecastController.cs
+++ RestfullAPINET/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace RestfullAPINET.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+          "Quockhanh1","QuocKhanh2","QuocKhanh3"  
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
